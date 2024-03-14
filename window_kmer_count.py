12e2a4e3db005a04e9a03b6a32f9a9693c26c8f7
MattesMrzik <mattes@mrzik.de>
2024-03-14 17:37:43 -0400

    simple detecting if consecutive kmers are present in different frames

--- window_kmer_count.py
+++ window_kmer_count.py
@@ -1,78 +1,68 @@
-seq = ("TAT"
-       "ACGACGACG"
-       "GGT"
-       "ACGACGACG"
-       "GGTGGTGGT"
-       "GTAGTGGGTGAGTGCGTGC")
+import argparse
 
 
-class CustomQueue:
-    def __init__(self, k):
-        self.k = k
-        queue_size = 1
-        self.queues = [["$"] * queue_size for _ in range(k)]
-        self.frame = 0
-        self.scores = [[] * queue_size for _ in range(k)]
-
-    def enqueue(self, kmer):
-
-        queue = self.queues[self.frame]
-        # print("frame", self.frame, "kmer", kmer, "queue", queue, "score", kmer in queue)
-        if kmer in queue:
-            self.scores[self.frame].append(1)
+def get_repeat(k, seq, frame):
+    result = seq[:frame]
+    n_found = 0
+    for i in range(frame, len(seq), k):
+        current_kmer = seq[i: i + k]
+        next_kmer = seq[i + k: i + k * 2]
+        if current_kmer == next_kmer:
+            n_found += 1
         else:
-            self.scores[self.frame].append(0)
-
-        if len(queue) >= 1:  # and kmer not in queue:
-            queue.pop(0)
-            queue.append(kmer)
-        self.frame = (self.frame + 1) % self.k
-
-    def get_repeat(self, sequence):
-        for frame in range(self.k):
-            result = ""
-            last_kmer = ""
-            for i in range(len(self.scores[frame])):
-                position_in_seq = i * self.k + frame
-                current_kmer = sequence[position_in_seq: position_in_seq + self.k]
-                next_kmer = sequence[position_in_seq + self.k: position_in_seq + self.k * 2]
-
-                if self.scores[frame][i] == 0:
-                    if current_kmer == last_kmer:
-                        last_kmer = ""
-                    else:
-                        result += current_kmer[::-1]
-                if self.scores[frame][i] == 1:
-                    if last_kmer == current_kmer:
-                        result = result[:-2 - k - 2] + " " + str(int(result[-4 - k]) + 1)
-                    else:
-                        result += " 2_)" + current_kmer[::-1] + "("
-
-                last_kmer = current_kmer
-
-            print("k =", k, "frame =", frame)
-            print(result[::-1])
+            if n_found > 0:
+                result += "(" + current_kmer + ")_" + str(n_found + 1) + " "
+                n_found = 0
+            else:
+                result += current_kmer
+    if n_found > 0:
+        result += "(" + current_kmer + ")_" + str(n_found + 1) + " "
+    return result
 
 
-kmers_to_check = [2, 3, 4, 5]
-custom_queues = {k: CustomQueue(k) for k in kmers_to_check}
-max_k = max(kmers_to_check)
-last_max_kmer = "$" * max_k
-for base in seq:
-    last_max_kmer = last_max_kmer[1:] + base
-    for k in kmers_to_check:
-        current_kmer = last_max_kmer[-k:]
-        if "$" not in current_kmer:
-            custom_queues[k].enqueue(current_kmer)
+def read_sequences(fasta_file, file_format = "fasta"):
+    from Bio import SeqIO
+    seqs = []
+    for record in SeqIO.parse(fasta_file, file_format):
+        seqs.append(str(record.seq))
+    return seqs
 
-for k, queue in custom_queues.items():
-    print(seq)
-    # print(" " * (k - 2), "".join([str(i) for i in queue.score]))
-    # for i in range(len(queue.score) - k - k + 1):
-    #     if queue.score[i + k + k - 1] != 0 and queue.score[i] == 0:
-    #         queue.score[i:i + k + k - 1] = [queue.score[i + k + k - 1]] * (k + k - 1)
-    queue.get_repeat(seq)
-    # print("####")
-    # mask = "".join([" " if i == 0 else str(k) for i in queue.score])
-    # print(" " * (k - 2), mask)
-    # print()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description = "shows kmer repeats in a sequence. Use only one sequence option at a time")
+    parser.add_argument("--sequence", help = "sequence to be analyzed")
+    # --seq AGTGACGACGACGTGGAGGGAGTAGTAGTAGCTACGT
+    parser.add_argument("--fasta", help = "file with sequence to be analyzed in fasta format")
+    parser.add_argument("--fastq", help = "file with sequence to be analyzed in fastq format")
+    parser.add_argument("-k", help = "kmer size. if none provided k = 3,4,5 will be used.", type = int, default = -1)
+
+    args = parser.parse_args()
+
+    if not any([args.sequence, args.fasta, args.fastq]):
+        print("you must provide a sequence")
+        exit()
+    if sum([bool(args.sequence), bool(args.fasta), bool(args.fastq)]) > 1:
+        print("only one sequence option is allowed")
+        exit()
+
+    if args.sequence:
+        seqs = [args.sequence]
+    elif args.fasta:
+        seqs = read_sequences(args.fasta)
+    elif args.fastq:
+        seqs = read_sequences(args.fastq, "fastq")
+
+    if args.k == -1:
+        ks = list(range(3, 6))
+    else:
+        ks = [args.k]
+    print("running for k =", ks)
+    for seq in seqs:
+        for k in ks:
+            for frame in range(k):
+                print("the input sequence is:")
+                print(seq);
+                print(f"the found {k}-mer repeats in frame {frame} are:")
+                print(get_repeat(k, seq, frame))
+                print()
